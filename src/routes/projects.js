d798ffabeca889f0f6851d6a2640c1331db2df80
David Javier Monroy <davmonroyp@MacBook-Air.local>
2021-02-25 12:38:35 -0400

    Base de anadir proyectos

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,8 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+router.get('/projects/add', (req, res) => {
+    res.render('projects/new-project');
+});
+
+router.post('/projects/new-project', (req, res) => {
+    const { name, description } = req.body;
+    const errors = [];
+    if (!name){
+        errors.push({text: 'Por favor inserte un nombre'});
+    }
+    if (!description){
+        errors.push({text: 'Por favor ingrese una descripcion'});
+    }
+    if(errors.length > 0){
+        res.render('projects/new-project', {
+            errors,
+            name,
+            description
+        });
+    }else{
+        res.send('OK');
+    }
+});
+
 router.get('/projects', (req, res) => {
-    res.send('Projects');
+    res.send('Bien');
 });
 
 module.exports = router;
